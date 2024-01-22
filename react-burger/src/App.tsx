bce58d62a1d6524301b3a0b92b221f4910465d9c
Rustem.Diveev <rustem.diveev@glowbyteconsulting.com>
2024-01-22 10:00:52 +0300

    new: dev: Старт верстки табов и оставшихся двух компонентов

--- react-burger/src/App.tsx
+++ react-burger/src/App.tsx
@@ -2,11 +2,35 @@ import AppHeader from "./components/app-header/app-header";
 import BurgerIngredients from "./components/burger-ingredients/burger-ingredients";
 import Sbx from "./components/sbx/sbx";
 
+
+const mainStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignContent: "center"
+}
+
+// Если убрать React.CSSProperties - то ругается на неправильное значение flexDirection
+const sectionStyle = {
+  display: "flex",
+  flex: 1,
+  justifyContent: "center",
+  flexDirection: "column",
+} as React.CSSProperties
+
 function App() {
   return (
     <>
-      <AppHeader/>
-      <BurgerIngredients/>
+      <header>
+        <AppHeader/>
+      </header>
+      <main style={mainStyle}>
+        <section style={sectionStyle}>
+          <BurgerIngredients/>
+        </section>
+        <section style={sectionStyle}>
+          <BurgerIngredients/>
+        </section>
+      </main>
     </>
   );
 }
--- react-burger/src/components/burger-ingredients/burger-ingredients.js
+++ react-burger/src/components/burger-ingredients/burger-ingredients.js
@@ -1,6 +1,37 @@
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components"
+import data from "../../utils/data"
+
+
+const ulStyle = {
+  display: "flex",
+  listStyle: "none"
+}
+
 const BurgerIngredients = () => {
   return (
-    <h1>BurgerIngredients</h1>
+    <>
+      <div style={{display: "flex"}}>
+        <Tab value="puns" active={true} key={1}>
+          Булки
+        </Tab>
+        <Tab value="sauces" key={2}>
+          Соусы
+        </Tab>
+        <Tab value="toppings" key={3}>
+          Начинки
+        </Tab>
+      </div>
+      <div style={{display: "flex"}}>
+        <h1 className="text text_type_main-default">Булки</h1>
+        <ul>
+          {data.filter(item => item.type === "bun").map(item => {
+              return (
+                <li>{item.name}</li>
+              )
+          })}
+        </ul>
+      </div>
+    </>
   )
 }
 
