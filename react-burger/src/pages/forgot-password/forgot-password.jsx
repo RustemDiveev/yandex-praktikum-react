afd245ec36449c8f7f878fd0958a233194138daf
Rustem.Diveev <rustem.diveev@glowbyteconsulting.com>
2024-03-23 17:52:35 +0300

    new: dev: Вызов сервиса по отправке письма для восстановления пароля

--- react-burger/src/pages/forgot-password/forgot-password.jsx
+++ react-burger/src/pages/forgot-password/forgot-password.jsx
@@ -1,11 +1,43 @@
+import { useRef } from "react"
+
+import { useNavigate } from "react-router-dom"
+
 import { Input, Button } from "@ya.praktikum/react-developer-burger-ui-components"
 
 import AppHeader from "../../components/app-header/app-header"
-
 import styles from "./forgot-password.module.css"
+import { PASSWORD_RESET_EMAIL_URL } from "../../settings/urls"
 
 
 const ForgotPassword = () => {
+  
+  const emailRef = useRef(null)
+  const navigate = useNavigate()
+
+  const onClickRestore = async () => {
+    const email = emailRef.current.value ?? ""
+    try {
+      const response = await fetch(
+        PASSWORD_RESET_EMAIL_URL, 
+        {
+          method: "POST",
+          body: {email}
+        }
+      )
+      if (response.ok) {
+        const responseData = await response.json()
+        if (responseData.success) {
+          navigate("/reset-password")
+        }
+      }
+      else {
+        console.error("Password reset request finished with error")
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <>
       <AppHeader />
@@ -13,21 +45,25 @@ const ForgotPassword = () => {
         <section className={styles.section}>
           <p className="text text_type_main-medium mb-3">Восстановление пароля</p>
           <Input 
+            ref={emailRef}
             type={"text"}
             placeholder={"Укажите e-mail"}
-            extraClass="mt-3 mb-3"
+            extraClass="mt-3 mb-3"            
           />
           <Button 
             htmlType="button" 
             type="primary" 
             size="large"
             extraClass="mt-3 ml-30 mr-30"
+            onClick={onClickRestore}
           >
             Восстановить
           </Button>
           <p className="text text_type_main-default mt-20 mb-2">
             Вспомнили пароль?&nbsp;
-            <a href="#">Войти</a>
+            <Button htmlType="button" type="secondary" size="medium">
+              Войти  
+            </Button>
           </p>
         </section>
       </main>
--- react-burger/src/settings/urls.js
+++ react-burger/src/settings/urls.js
@@ -3,3 +3,5 @@ export const API_URL = "https://norma.nomoreparties.space/api"
 export const ORDERS_URL = `${API_URL}/orders/`
 
 export const INGREDIENTS_URL = `${API_URL}/ingredients`
+
+export const PASSWORD_RESET_EMAIL_URL = `${API_URL}/password-reset`
