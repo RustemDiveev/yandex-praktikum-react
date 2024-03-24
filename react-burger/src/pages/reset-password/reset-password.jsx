04ea4276cd2445c365003176af0e71fc5ec84151
Rustem Diveev <rustem.diveev@gmail.com>
2024-03-24 13:47:41 +0300

    new: dev: доделанный 2 пункт со сбросом пароля

--- react-burger/src/pages/reset-password/reset-password.jsx
+++ react-burger/src/pages/reset-password/reset-password.jsx
@@ -1,11 +1,48 @@
+import { useRef } from "react"
+
+import { useNavigate } from "react-router-dom"
+
 import { Input, Button } from "@ya.praktikum/react-developer-burger-ui-components"
 
 import AppHeader from "../../components/app-header/app-header"
+import { PASSWORD_RESET_URL } from "../../settings/urls"
 
 import styles from "./reset-password.module.css"
 
 
 const ResetPassword = () => {
+
+  const passwordRef = useRef(null)
+  const tokenRef = useRef(null)
+  const navigate = useNavigate()
+
+  const resetPassword = async () => {
+    try {
+      const response = await fetch(
+        PASSWORD_RESET_URL,
+        {
+          method: "POST",
+          body: {
+            password: passwordRef.current.value,
+            token: tokenRef.current.value
+          }
+        }
+      )
+      if (response.ok) {
+        await response.json()
+      }
+      else {
+        console.error("Password reset request finished with error")
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const toLogin = () => {
+    navigate("/login")
+  }
+
   return (
     <>
       <AppHeader />
@@ -17,23 +54,33 @@ const ResetPassword = () => {
             placeholder={"Введите новый пароль"}
             icon={"ShowIcon"}
             extraClass="mt-3 mb-3"
+            ref={passwordRef}
           />
           <Input 
             type={"text"}
             placeholder={"Введите код из письма"}
             extraClass="mt-3 mb-3"
+            ref={tokenRef}
           />
           <Button 
             htmlType="button" 
             type="primary" 
             size="large"
             extraClass="mt-3 ml-30 mr-30"
+            onClick={resetPassword}
           >
             Сохранить
           </Button>
           <p className="text text_type_main-default mt-20 mb-2">
             Вспомнили пароль?&nbsp;
-            <a href="#">Войти</a>
+            <Button
+              htmlType="button"
+              type="secondary"
+              size="medium"
+              onClick={toLogin}
+            >
+              Войти
+            </Button>
           </p>
         </section>
       </main>
--- react-burger/src/settings/urls.js
+++ react-burger/src/settings/urls.js
@@ -5,3 +5,5 @@ export const ORDERS_URL = `${API_URL}/orders/`
 export const INGREDIENTS_URL = `${API_URL}/ingredients`
 
 export const PASSWORD_RESET_EMAIL_URL = `${API_URL}/password-reset`
+
+export const PASSWORD_RESET_URL = `${PASSWORD_RESET_EMAIL_URL}/reset`
