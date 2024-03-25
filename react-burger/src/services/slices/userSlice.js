c5c24b5cc74317149809c7e9c634b0221969a37e
Rustem Diveev <rustem.diveev@gmail.com>
2024-03-25 22:38:48 +0300

    new: dev: Часть 3-го пункта

--- react-burger/src/services/slices/userSlice.js
+++ react-burger/src/services/slices/userSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 
-import { REGISTER_URL } from "../../settings/urls"
+import { REGISTER_URL, LOGIN_URL } from "../../settings/urls"
 
 import requestApi from "../../utils/api"
 
@@ -18,11 +18,34 @@ const initialState = {
 export const registerUser = createAsyncThunk(
   "user/register",
   async ({email, password, name}) => {
+    const payload = {email, password, name}
+    console.log("payload: ", payload)
     const response = await requestApi(
       REGISTER_URL, 
       {
         method: "POST",
-        body: {email, password, name}
+        headers: {
+          "Content-Type": "application/json;charset=utf-8"
+        },
+        body: JSON.stringify(payload)
+      }
+    )
+    const responseJson = response.json()
+    return responseJson
+  }
+)
+
+export const loginUser = createAsyncThunk(
+  "user/login",
+  async ({email, password}) => {
+    const response = await requestApi(
+      LOGIN_URL,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8"
+        },
+        body: JSON.stringify({email, password})
       }
     )
     const responseJson = response.json()
@@ -35,12 +58,19 @@ const userSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers(builder) {
-    builder.addCase(registerUser.fulfilled, (state, action) => {
-      state.success = action.payload.success
-      state.user = action.payload.user 
-      state.accessToken = action.payload.accessToken
-      state.refreshToken = action.payload.refreshToken
-    })
+    builder
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.success = action.payload.success
+        state.user = action.payload.user 
+        state.accessToken = action.payload.accessToken
+        state.refreshToken = action.payload.refreshToken
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.success = action.payload.success
+        state.user = action.payload.user 
+        state.accessToken = action.payload.accessToken
+        state.refreshToken = action.payload.refreshToken
+      })
   }
 })
 
